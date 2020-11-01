bf7decaead5f0833d1303b3fddbd2416fe1508a7
unknown <jurakhodjayev2000@mail.ru>
2020-11-01 12:17:24 +0500

    Easy

---
+++ Linked List Cycle/index.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        turtle = head
+        hare = head
+        while turtle and hare and hare.next:
+            hare = hare.next.next
+            turtle = turtle.next
+            if turtle == hare:
+                return True
+        
+        return False
